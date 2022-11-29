48e80c091cd1a1bdc5bb253e50df1c454986f895
bukiito <86784109+bukiito@users.noreply.github.com>
2022-11-29 11:21:18 +0300

    Update questions.js

--- Quiz Application with Timer/js/questions.js
+++ Quiz Application with Timer/js/questions.js
@@ -43,7 +43,8 @@ let questions = [
       "Buttons",
       "Stand Up For Love"
     ]
-  },
+  }
+      
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
